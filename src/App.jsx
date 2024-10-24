d707f3429e4ece882bca036a12bd1b32de567ce7
hasibprangon <hasibulhossainp09@gmail.com>
2024-10-25 03:02:27 +0600

    adding fake data and loading data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-4xl bg-red-400'>Knowledge cafe</h1>
+     <Header></Header>
+     <Blogs></Blogs>
       
     </>
   )
