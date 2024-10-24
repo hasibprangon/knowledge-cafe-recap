419b969c5e2b3eca06bd3c3225c98d207f31f012
hasibprangon <hasibulhossainp09@gmail.com>
2024-10-25 03:35:24 +0600

    adding some style and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmark/Bookmarks'
 import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-     <Header></Header>
-     <Blogs></Blogs>
-      
+      <Header></Header>
+      <main className='md:flex container mx-auto mt-3'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </main>
+
     </>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import profile from '../../../public/images/profile.png'
 
 const Header = () => {
     return (
-        <div>
-             <h1 className='text-4xl font-bold'>Knowledge cafe</h1>
-        </div>
+        <header className='container mx-auto flex justify-between items-center border-b-2  p-2'>
+             <h1 className='text-3xl font-semibold'>Knowledge cafe</h1>
+             <img className='w-11' src={profile} alt="" />
+        </header>
     );
 };
 
